3440b5cdcf63935079a922f963b031f47c3ee0db
jamesmthornton68 <james-thornton@hotmail.com>
2020-12-23 11:27:27 +0000

    commit files in demotest22Dec folder

---
+++ src/main/java/com/simplilearn/calculator/demotest22Dec/Demotest22DecApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.calculator.demotest22Dec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demotest22DecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demotest22DecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/calculator/demotest22Dec/Demotest22DecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.calculator.demotest22Dec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demotest22DecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
